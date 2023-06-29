4abdd474128816686fd0469c2952ef94d57bbd45
Syed Hussain Ather <shussainather@gmail.com>
2023-06-29 16:56:53 -0400

    first commit

---
+++ src/ApiComponent.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ApiComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://rapidapi.com/Glavier/api/twitter135/');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>API Component</h1>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+async function getTwitterData(query) {
+  const options = {
+    method: 'GET',
+    url: 'https://twitter135.p.rapidapi.com/AutoComplete/',
+    params: { q: query },
+    headers: {
+      'X-RapidAPI-Key': 'YOUR_RAPIDAPI_KEY',
+      'X-RapidAPI-Host': 'twitter135.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export default ApiComponent;
+
